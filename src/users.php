d733e0a2668a37dc6dfa8e5d14d396f04aa17b22
Val <valentin.leon0@gmail.com>
2016-02-05 12:04:01 +0100

    Ajout de la classe user

--- src/users.php
+++ src/users.php
@@ -8,5 +8,56 @@
  */
 class users
 {
+    private $id_user;
+
+    private $status;
+
+    private $name;
+
+    private $firstname;
+
+    private $email;
+
+    private $atCreate;
+
+    private $atUpdate;
+
+    private $id_class;
+
+
+    public function getIdUser()
+    {
+        return $this->id_user;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getAtCreate()
+    {
+        return $this->atCreate;
+    }
+
+    public function getAtUpdate()
+    {
+        return $this->atUpdate;
+    }
 
 }
