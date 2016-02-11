20496a2be481d1215c7fc48f9ed0dfb8632aa110
PlanesZwalker <anne@jadeau.fr>
2016-02-11 21:41:22 +0100

    1- option de configuration de l'application liée à la mise en production / connexion à la db

---
+++ app/config/prod.php
@@ -0,0 +1,22 @@
+<?php
+
+
+// Doctrine (db)
+
+$app['db.options'] = array(
+
+    'driver'   => 'pdo_mysql',
+
+    'charset'  => 'utf8',
+
+    'host'     => '162.243.111.161',
+
+    'port'     => '8082',
+
+    'dbname'   => 'classmanager',
+
+    'user'     => 'groupe_sio2',
+
+    'password' => 'groupe_sio2',
+
+);
