bc449aaedc062178d89c021274d5bc89b2d97726
NielsFILSKOV <niels.f@hotmail.fr>
2016-02-11 14:11:34 +0100

    modification des requette sql + commentaires

--- db/className.sql
+++ db/className.sql
@@ -1,12 +1,11 @@
 /*Création de la table className*/
 
 CREATE TABLE className(
-    id_class        INT(11) PRIMARY KEY NOT NULL,
-    class_name      VARCHAR(10),
-    class_option    VARCHAR(10), /*Définit l'option de l'élève*/
-    class_year      SMALLINT, /*Définit l'année de la classe*/
-    dt_create       DATE NOT NULL, /*Date de création*/
-    dt_update       DATE NOT NULL, /*Date de modification*/
-    id_student      INT NOT NULL,
-    PRIMARY KEY (id_student)REFERENCES student(id_student),
+    id_class        INT(11) NOT NULL ,
+    class_name      VARCHAR(50) ,
+    class_option    VARCHAR(50) , /*Définit l'option de l'élève*/
+    class_year      SMALLINT , /*Définit l'année de la classe*/
+    dt_create       DATE NOT NULL , /*Date de création*/
+    dt_update       DATE NOT NULL , /*Date de modification*/
+    PRIMARY KEY (id_class) ,
 )
--- db/student.sql
+++ db/student.sql
@@ -1,12 +1,14 @@
 /*Création de la table student*/
 
 CREATE TABLE student(
-        id_student              INT (11) Auto_increment PRIMARY KEY NOT NULL ,
+        id_student              INT (11) Auto_increment NOT NULL ,
         student_name            VARCHAR (25) ,
         student_firstname       VARCHAR (50) ,
         student_birthday        DATE ,
+        student_email           VARCHAR (255) ,
         dt_create               DATE NOT NULL , /*Date de création*/
         dt_update               DATE NOT NULL , /*Date de modification*/
-        student_email           VARCHAR (255) ,
-        FOREIGN KEY (id_class) REFERENCES className(id_class),
+        id_evaluation           INT NOT NULL ,
+        PRIMARY KEY (id_student) ,
+        FOREIGN KEY (id_evaluation) REFERENCES evaluation(id_evaluation)
 )ENGINE=InnoDB;
