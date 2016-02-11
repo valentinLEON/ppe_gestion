2d4150c6cbb9a9d5b8048a13e6016e177084f9ee
Singu_Admin <valentin.leon0@gmail.com>
2016-02-11 21:13:38 +0100

    Création des fichiers dev.php et prod.php pour la connexion à la base de données en se servant de l'orm

---
+++ app/config/dev.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Singu_Admin
+ * Date: 11/02/2016
+ * Time: 21:11
+ */
+
+require __DIR__.'/prod.php';
+
+//activation du mode debug, pour développeur
+$app['debug'] = true;
---
+++ app/config/prod.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Singu_Admin
+ * Date: 11/02/2016
+ * Time: 21:07
+ */
+
+//paramétrage de la connexion à la base de données avec l'orm DBAL
+$app['db.options'] = array(
+    'driver' => 'pdo_mysql',
+    'charset' => 'utf8',
+    'host' => 'localhost',
+    'port' => '3306',
+    'dbname' => 'groupe_sio2',
+    'user' => 'groupe_sio2',
+    'password' => 'groupe_sio2', //ce qui n'est pas forcément sécurisé !
+);
