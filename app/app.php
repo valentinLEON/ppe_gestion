28ca542402e03427ab03001a52111f25bb6e9379
PlanesZwalker <anne@jadeau.fr>
2016-02-12 09:45:08 +0100

    Update app.php

--- app/app.php
+++ app/app.php
@@ -1,18 +1,18 @@
 <?php
-<<<<<<< Updated upstream
-
 namespace ppe_project_gestion;
 
-=======
 /**
  * Created by PhpStorm.
  * User: Val
  * Date: 05/02/2016
  * Time: 09:09
  */
->>>>>>> Stashed changes
+
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
+use Symfony\Component\HttpFoundation\Request;
+use Silex\Application;
+use Silex\ControllerProviderInterface;
 
 // Register global error and exception handlers
 ErrorHandler::register();
@@ -20,7 +20,24 @@ ExceptionHandler::register();
 
 // Register service providers.
 $app->register(new Silex\Provider\DoctrineServiceProvider());
-<<<<<<< Updated upstream
+
+
+class MyClassController implements ControllerProviderInterface {
+ 
+  public function connect(Application $app) {
+    return $app['controllers_factory'];
+  }
+     public function connect(Application $app) {
+      $factory=$app['controllers_factory'];
+      // Routes are defined here
+      $factory->get('/','MyApp\MyClassController::home');
+      return $factory;
+    }
+     
+    public function home() {
+      return 'Hello world';
+    }
+}
 
 // Register services.
 $app['dao.student'] = $app->share(function ($app) {
