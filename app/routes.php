3a2c7e75e52ad7b5fe1b2b8c29d1d9b2d7fc2584
guillaume <gcoutard27@gmail.com>
2016-02-12 09:05:04 +0100

    test view 2

--- app/routes.php
+++ app/routes.php
@@ -4,8 +4,6 @@
 $app->get('/', function () use ($app) {
     $students = $app['dao.student']->findAll();
 
-    ob_start();             // start buffering HTML output
     require '../views/students.php';
-    $view = ob_get_clean(); // assign HTML output to $view
-    return $view;
+    return $app['twig']->render('index.html.twig', array('student' => $students));
 });
