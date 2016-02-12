60e141338daf1b3682c3998bb3237fe3069111ab
Val <valentin.leon0@gmail.com>
2016-02-12 12:17:44 +0100

    Création de la table USERS

---
+++ db/users.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS users;
+
+/*Création de la table des utilisateurs*/
+
+CREATE TABLE users(
+  `id_users`        INT(11) NOT NULL ,
+  `user_name`       VARCHAR(50) NOT NULL,
+  `user_password`   VARCHAR(50) NOT NULL,
+  `user_hash`       VARCHAR(23) NOT NULL, /* Hashage du mot de passe */
+  `user_role`       VARCHAR(25) NOT NULL, /* Définit le rôle de l'utilisateur */
+  `dt_create`       DATE NOT NULL , /*Date de création*/
+  `dt_update`       DATE NOT NULL , /*Date de modification*/
+  `id_discipline`   INT(11) NOT NULL,
+  `id_class`        INT(11) NOT NULL,
+  PRIMARY KEY (id_users),
+  FOREIGN KEY (id_discipline) REFERENCES discipline(id_discipline),
+  FOREIGN KEY (id_class) REFERENCES className(id_class)
+)ENGINE=innodb CHARACTER SET utf8 COLLATE utf8_unicode_ci;
