a00157871cf233399eb6bd138e14bd4a77ed0c83
PlanesZwalker <anne@jadeau.fr>
2016-02-14 17:43:20 +0100

    ajout des composants et services

--- app/app.php
+++ app/app.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,27 +6,21 @@
  * Date: 05/02/2016
  * Time: 09:09
  */
-
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
-//use Symfony\Component\HttpFoundation\Request;
-//use Silex\Application;
-//use Silex\ControllerProviderInterface;
-
+use Symfony\Component\HttpFoundation\Request;
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
+use Silex\ControllerProviderInterface;
+// Register global error and exception handlers
 ErrorHandler::register();
 ExceptionHandler::register();
-
 // Register service providers.
 $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
+    'twig.class_path' => __DIR__.'/../vendor/twig/twig/lib',
+    'twig.options' => array(),
 ));
-
-
-// Register services.
-/*$app['dao.className'] = $app->share(function ($app) {
-    return new ppe_project_gestion\DAO\ClassNameDAO($app['db']);
-});*/
-
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 return $app;
-
