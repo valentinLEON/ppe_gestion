e9a9397156f4976d50fc22fb2da7522ef510b31e
Val <valentin.leon0@gmail.com>
2016-02-18 15:23:01 +0100

    Ajout de l'attribut description dans la requête des users

--- db/users.sql
+++ db/users.sql
@@ -6,8 +6,9 @@ CREATE TABLE users(
   `id_users`        INT(11) NOT NULL ,
   `username`        VARCHAR(50) NOT NULL,
   `password`        VARCHAR(50) NOT NULL,
-  `salt`       VARCHAR(23) NOT NULL, /* Hashage du mot de passe */
+  `salt`            VARCHAR(23) NOT NULL, /* Hashage du mot de passe */
   `roles`           VARCHAR(23) NOT NULL, /* Définit le rôle de l'utilisateur */
+  `description`     VARCHAR(100) NOT NULL, /* Décrit la fonction de l'utilisateur */
   `dt_create`       DATE NOT NULL , /*Date de création*/
   `dt_update`       DATE NOT NULL , /*Date de modification*/
   `id_discipline`   INT(11) NOT NULL,
