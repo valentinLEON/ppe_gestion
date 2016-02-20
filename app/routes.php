b5607370bee4cc55801b59657510b105ac680277
Singu_Admin <valentin.leon0@gmail.com>
2016-02-20 12:04:46 +0100

    test

--- app/routes.php
+++ app/routes.php
@@ -4,15 +4,15 @@ use Symfony\Component\HttpFoundation\Request;
 
 //route pour le formulaire d'ajout de note
 $app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
+
+$app->get('/addstudent',function() use ($app) {
     $classes = $app['dao.className']->findAll();
     $discipline = $app['dao.discipline']->findAll();
     $etudiant = $app['dao.student']->findall();
-    return $app['twig']->render('index.html.twig', array(
+    return $app['twig']->render('addstudent.html.twig', array(
         'classNames' => $classes,
         'matieres' => $discipline,
         'student' => $etudiant)); //
-});
-
-$app->get('/addstudent',function(Request $request) use ($app) {
-    return $app['twig']->render('addstudent.html.twig', array() );
 })->bind('addstudent');
