a49efdd34e7d97b3dbc3d9ae391cfe6a1d7dac46
Singu_Admin <valentin.leon0@gmail.com>
2016-02-21 23:27:11 +0100

    Les routes manquantes pour les pages existantes.
    Sans fonctions de base, juste pour les afficher

--- app/routes.php
+++ app/routes.php
@@ -2,9 +2,44 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Route pour l'accueil
+ */
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
+
 //route pour le formulaire d'ajout de note
 $app->get('/', function () use ($app) {
     return $app['twig']->render('addnote.html.twig');
+});
+
+/**
+ * route pour l'ajout des étudiants
+ */
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('addstudent.html.twig');
+});
+
+/**
+ * route pour l'ajout des utilisateurs
+ */
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('adduser.html.twig');
+});
+
+/**
+ * route pour afficher le calendrier
+ */
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('calendar.html.twig');
+});
+
+/**
+ * route pour afficher le login
+ */
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('login.html.twig');
 });
 
 $app->get('/addnote',function() use ($app) {
