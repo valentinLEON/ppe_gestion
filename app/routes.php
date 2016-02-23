d44bf1e5dcc9b9386dcbc64c7a1cca522027f5da
Val <valentin.leon0@gmail.com>
2016-02-23 11:07:18 +0100

    Pour Niels, regarde et comprends...
    Sur openclassroom, c'est itération 9

--- app/routes.php
+++ app/routes.php
@@ -1,9 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-use ppe_gestion\Domain\Student;
-use ppe_gestion\Domain\Discipline;
-use ppe_gestion\Domain\ClassName;
+use ppe_gestion\Form\Type\addNoteForm;
 
 /**
  * TODO: faire les routes pour afficher les listes des datas
@@ -19,21 +17,21 @@ $app->get('/', function () use ($app) {
 /**
  * route pour l'ajout des étudiants
  */
-$app->get('addstudent', function () use ($app) {
+$app->get('/addstudent', function () use ($app) {
     return $app['twig']->render('Formulaires/addstudent.html.twig');
 })->bind('addstudent');
 
 /**
  * route pour l'ajout des utilisateurs
  */
-$app->get('adduser', function () use ($app) {
+$app->get('/adduser', function () use ($app) {
     return $app['twig']->render('Formulaires/adduser.html.twig');
 })->bind('adduser');
 
 /**
  * route pour afficher le calendrier
  */
-$app->get('calendar', function () use ($app) {
+$app->get('/calendar', function () use ($app) {
     //$eval = $app['dao.evaluation']->findAllByDiscipline(1);
     return $app['twig']->render('calendar.html.twig');
 })->bind('calendar');
@@ -41,7 +39,7 @@ $app->get('calendar', function () use ($app) {
 /**
  * route pour afficher le login
  */
-$app->get('login', function (Request $request) use ($app) {
+$app->get('/login', function (Request $request) use ($app) {
     return $app['twig']->render('login.html.twig', array(
         'error' => $app['security.last.error']($request),
         'last_username' => $app['session']->get('_security.last_username'),
@@ -51,24 +49,33 @@ $app->get('login', function (Request $request) use ($app) {
 /**
  * route pour l'ajout des classes
  */
-$app->match('addclass', function () use ($app) {
+$app->match('/addclass', function () use ($app) {
     return $app['twig']->render('Formulaires/addclass.html.twig');
 })->bind('addclass');
 
 /**
  * route pour l'ajout des matières
  */
-$app->get('adddiscipline', function () use ($app) {
+$app->get('/adddiscipline', function () use ($app) {
     return $app['twig']->render('Formulaires/adddiscipline.html.twig');
 })->bind('adddiscipline');
 
 /**
  * Route pour l'ajout des notes
  */
-$app->get('addnote',function() use ($app) {
+$app->match('/addnote',function(Request $request) use ($app) {
     $classes = $app['dao.className']->findAll();
     $discipline = $app['dao.discipline']->findAll();
     $etudiant = $app['dao.student']->findall();
+    $noteFormView = null;
+    $note = new \ppe_gestion\Domain\Evaluation();
+    $noteForm = $app['form.factory']->create(new addNoteForm(), $note);
+    $noteForm->handleRequest($request);
+    if($noteForm->isSubmitted() && $noteForm->isValid())
+    {
+        $app['dao.evaluation']->save($note);
+    }
+    $noteFormView = $noteForm->createView();
     return $app['twig']->render('Formulaires/addnote.html.twig', array(
         'classNames' => $classes,
         'matieres' => $discipline,
---
+++ src/Form/Type/addNoteForm.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Val
+ * Date: 23/02/2016
+ * Time: 10:20
+ */
+
+namespace ppe_gestion\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class addNoteForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('grade', ChoiceType::class)
+        ;
+    }
+
+    public function getName()
+    {
+        return 'note';
+    }
+}
