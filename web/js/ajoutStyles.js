c1f6691531d2326d0f9853867457b2eccf9bb156
PlanesZwalker <anne@jadeau.fr>
2016-02-24 03:55:52 +0100

    ajout js

---
+++ web/js/ajoutStyles.js
@@ -0,0 +1,12 @@
+
+function bgOpacitymax(){
+    
+    var btnAdmin=getElementById('btnAdmin');
+    btnAdmin.style.opacity='1';
+}
+
+function bgOpacitymin(){
+    
+    var btnAdmin=getElementById('btnAdmin');
+    btnAdmin.style.opacity='0.7';
+}
