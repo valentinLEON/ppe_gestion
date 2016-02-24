d1af7f3d0926356ca2c2b2522b5a911674937ca4
PlanesZwalker <anne@jadeau.fr>
2016-02-24 04:20:48 +0100

    js

--- web/js/ajoutStyles.js
+++ web/js/ajoutStyles.js
@@ -3,14 +3,18 @@ function bgOpacitymax(){
     
     var btnAdmin=document.getElementById('btnAdmin');
     btnAdmin.style.opacity='1';
+    btnAdmin.style.border='0px solid gray';
     btnAdmin.style.padding='0px';
+    btnAdmin.style.cursor='pointer';
 }
 
 function bgOpacitymin(){
     
     var btnAdmin=document.getElementById('btnAdmin');
     btnAdmin.style.opacity='0.7';
-    btnAdmin.style.padding='1px';
+    btnAdmin.style.border='2px solid gray';
+    btnAdmin.style.padding='-2px';
+    btnAdmin.style.cursor='pointer';
 }
 
 function showAdminAcces(){
