9f18b870a83ff213d383f2430a6889e9e29a2078
PlanesZwalker <anne@jadeau.fr>
2016-02-24 04:00:13 +0100

    js

--- web/js/ajoutStyles.js
+++ web/js/ajoutStyles.js
@@ -10,3 +10,10 @@ function bgOpacitymin(){
     var btnAdmin=getElementById('btnAdmin');
     btnAdmin.style.opacity='0.7';
 }
+
+function showAdminAcces(){
+    
+   var accesAdmin= getElementById('accesAdmin');
+       accesAdmin.style.visibility='visible';
+       accesAdmin.style.display='block';
+}
