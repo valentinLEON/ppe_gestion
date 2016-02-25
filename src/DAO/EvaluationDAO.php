d4a2a91fbde53635d981681cc90d3aa2ba0ef9a0
Singu_Admin <valentin.leon0@gmail.com>
2016-02-25 22:00:31 +0100

    Coucou

--- src/DAO/EvaluationDAO.php
+++ src/DAO/EvaluationDAO.php
@@ -126,7 +126,7 @@ class EvaluationDAO extends DAO
             '$dt_update'        => $evaluation->getDtUpdate()
         );
 
-        if($evaluation == getIdEvaluation())
+        if($evaluation->getIdEvaluation())
         {
             $this->getDb()->update('evaluation', $grade, array('id_evaluation'=> $evaluation->getIdEvaluation()));
         }
