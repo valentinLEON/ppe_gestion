3339ee66404473ebe73b52d2815b43105438c99a
NielsFILSKOV <niels.f@hotmail.fr>
2016-02-25 10:04:22 +0100

    test ajout note

--- src/Form/Type/addNoteForm.php
+++ src/Form/Type/addNoteForm.php
@@ -17,6 +17,6 @@ class addNoteForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('grade_student', 'number', array('max_length'=>20));
+        $builder->add('grade_student', 'integer', array('max_length'=>20));
     }
 }
