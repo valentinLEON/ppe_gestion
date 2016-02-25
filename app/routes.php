7ca38001aa6162015e4c894cd4a444074b362b5f
Singu_Admin <valentin.leon0@gmail.com>
2016-02-25 22:21:51 +0100

    Coucou

--- app/routes.php
+++ app/routes.php
@@ -294,8 +294,8 @@ $app->post('/addnote', function(Request $request) use ($app){
     $newEvaluation->setStudent($student);
     $newEvaluation->setCoefDiscipline(2);
     $newEvaluation->setJudgement('je suis un commentaire');
-    $newEvaluation->setDtCreate(getdate('Y-m-d'));
-    $newEvaluation->setDtUpdate(getdate('Y-m-d'));
+    $newEvaluation->setDtCreate(date('Y-m-d'));
+    $newEvaluation->setDtUpdate(date('Y-m-d'));
 
     $app['dao.evaluation']->saveGrade($newEvaluation);
     //$app['session']->getFlashBag()->add('success', 'La note a bien été ajouté !'); //message flash success si réussi
