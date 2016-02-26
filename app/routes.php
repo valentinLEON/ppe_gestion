f028b18a041049ebca8001f43b67c8b12c2d5e9f
PlanesZwalker <anne@jadeau.fr>
2016-02-26 07:01:13 +0100

    test

--- app/routes.php
+++ app/routes.php
@@ -158,9 +158,9 @@ $app->post('/adduser', function(Request $request) use ($app){
    
     $newUser = new User();
  
-    $user = $app['dao.users']->$request->request->get('user');
+    $user = $app['dao.users']-> setUsername($request->request->get('user'));
 
-    $newUser->setUsername($request->request->get('username'));
+    $newUser->setUsername($user);
     
     $newUser->setDtCreate(date('Y-m-d'));
     $newUser->setDtUpdate(date('Y-m-d'));
