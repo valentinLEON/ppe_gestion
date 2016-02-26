af54c9196bf4f5161e2bad5bad5893eedae97825
PlanesZwalker <anne@jadeau.fr>
2016-02-26 09:07:10 +0100

    dao

--- src/DAO/UserDAO.php
+++ src/DAO/UserDAO.php
@@ -122,9 +122,9 @@ class UserDAO extends DAO implements UserProviderInterface
         $user->setUserMail($row['user_mail']);
         $user->setDtCreate($row['dt_create']);
         $user->setDtUpdate($row['dt_update']);
-
-        $user->setIdDiscipline($row['id_discipline']);
-        $user->setIdClass($row['id_class']);
+//
+//        $user->setIdDiscipline($row['id_discipline']);
+//        $user->setIdClass($row['id_class']);
 
         return $user;
     }
