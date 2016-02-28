efee0ecb7111ffa0f008dc684d2b2b29f3b9e13c
PlanesZwalker <anne@jadeau.fr>
2016-02-28 16:00:18 +0100

    re tests userslist

--- app/routes.php
+++ app/routes.php
@@ -348,9 +348,9 @@ $app->get('/modifuser', function () use ($app) {
 
 $app->post('/modifuser', function (Request $request) use ($app) {
     
-    $iduser= $request->request->get('id_user_form');
-    $modification= $request->request->get('modification');    
-     $idclasse = $request->request->get('$id_class_form');
+     $iduser= $request->request->get('id_user_form');
+     $modification= $request->request->get('modification');    
+     $idclass = $request->request->get('$id_class_form');
      $iddiscipline = $request->request->get('$id_discipline');
      $idrole = $request->request->get('id_role'); 
      
@@ -362,7 +362,7 @@ $app->post('/modifuser', function (Request $request) use ($app) {
      
      $users_total = $app['dao.users']->countAll();
 
-     $id_class = $app['dao.classNames']->findClassname($idclasse);
+   //  $id_class = $app['dao.classNames']->findClassname($idclasse);
      $id_discipline = $app['dao.discipline']->findDiscipline($iddiscipline);
      $id_role = $app['dao.users']->find($idrole);
      $id_user_form = $app['dao.users']->find($iduser);
@@ -373,7 +373,7 @@ $app->post('/modifuser', function (Request $request) use ($app) {
         'username'      =>$username,
         'role'          =>$roles,
         'role'          =>$roles,
-        'id_class_form' =>$id_class,
+        'id_class_form' =>$idclass,
         'id_discipline' =>$id_discipline,
         'id_role'       =>$id_role,
         'id_user'       =>$id_users,
