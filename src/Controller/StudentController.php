34a2b6d123c486c5792387cd224f27993c637088
Singu_Admin <valentin.leon0@gmail.com>
2016-03-02 00:39:57 +0100

    test de suppression d'un étudiant

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -42,11 +42,9 @@ class StudentController {
     }
 
     public function deleteIndexAction(Request $request, $id, Application $app){
-        $etudiant = $app['dao.student']->deleteStudent($id);
+        $app['dao.student']->deleteStudent($id);
 
-        return $app['twig']->render('ListTemplate/studentslist.html.twig', array(
-            'students' => $etudiant,
-        ));
+        return $app['twig']->render('ListTemplate/studentslist.html.twig');
     }
     
     public function addIndexAction(Request $request ,Application $app) {
