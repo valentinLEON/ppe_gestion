0513a40b1636b591c64aa6d2b4c8979bc1f26a0a
Val <valentin.leon0@gmail.com>
2016-03-03 12:42:58 +0100

    driohremg

--- src/DAO/StudentDAO.php
+++ src/DAO/StudentDAO.php
@@ -174,7 +174,7 @@ class StudentDAO extends DAO
         if(array_key_exists('id_evaluation', $row))
         {
             $evaluationID = $row['id_evaluation'];
-            $evaluation = $this->evaluationDAO->findEvaluation($evaluationID);
+            $evaluation = $this->evaluationDAO->find($evaluationID);
             $student->setEvaluation($evaluation);
         }
 
